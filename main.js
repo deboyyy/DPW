d9ab00f4356b6cd79aeac9e7211aff827403004c
unknown <juniarfarindo7@gmail.com>
2022-03-15 17:38:42 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const greeting = {
+    message : "say Hi",
+    times : 4
+}
+greeting.message = "Hai ges";
+console.log(greeting.message)//=>"Hai ges"
+
+const gambar = ['logo ig', 'logo tiktok'];
+
+for (let i = 0; i < gambar.length; i++) {
+    if (gambar) {
+        console.log(gambar[i]);
+    }
+    else if (no) {
+        console.log('Tidak ada data!!!');
+    }
+}
+
+console.log(i);
